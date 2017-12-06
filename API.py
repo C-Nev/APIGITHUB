38f1ccd203e8dab861c99bab9ea9a4d03c4bf804
Conor Nevin <conevin@tcd.ie>
2017-12-06 11:34:18 +0000

    Add files via upload
    Slightly changes and progress

--- API.py
+++ API.py
@@ -3,11 +3,19 @@ import requests
 #namelist = []
 #namelist.append(input("Please enter Username:"))  #Stored in namelist[0]
 #namelist.append(input('Please enter Password:'))  #Stored in namelist[1]
-
-r = requests.get('https://api.github.com/user/followers', auth=('C-Nev', 'Paudidog123'))
+auth=('C-Nev', 'Paudidog123')
+r = requests.get('https://api.github.com/user', auth=auth)
 
 print (r.status_code)
 print (r.headers['content-type'])
 data = r.json()
-{k: data[0].get(k, None) for k in ('login')}
-print(k)
+#{k: data.get(k, None) for k in ('login')}
+print(data)
+print('\n')
+g = requests.get('https://api.github.com/user/followers', auth=auth)
+data2 = g.text
+for k, v in enumerate(data2):
+    if v=='l':
+        print("test")
+        while v != ',':
+            print (v)
