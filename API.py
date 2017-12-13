5a4d0f536e737ea86ca1dcfd5f7cd402dc1f463b
Conor Nevin <conevin@tcd.ie>
2017-12-13 17:09:04 +0000

    New API project
    Extended work on Python API project using code found online to format the JSON data.
    Then use matplotlib to create a pi chart of the percentage of following vs followers.
    Basic data visualization.

--- API.py
+++ API.py
@@ -1,21 +1,79 @@
 import requests
+import json
+import random
+import matplotlib.pyplot as plt
 
-#namelist = []
-#namelist.append(input("Please enter Username:"))  #Stored in namelist[0]
-#namelist.append(input('Please enter Password:'))  #Stored in namelist[1]
-auth=('C-Nev', 'Paudidog123')
-r = requests.get('https://api.github.com/user', auth=auth)
+class user:
+    lgn = 'NULL'
+    id = 'NULL'
+    name = 'NULL'
+    location = 'NULL'
+    repos = 'NULL'
+    userSince = 'NULL'
+    followers = 'NULL'
+    following = 'NULL'
+    followers_url = 'NULL'
+    following_url = 'NULL'
+    trackedUser = 'NULL'
+    printStat = 'NULL'
 
-print (r.status_code)
-print (r.headers['content-type'])
-data = r.json()
-#{k: data.get(k, None) for k in ('login')}
-print(data)
-print('\n')
-g = requests.get('https://api.github.com/user/followers', auth=auth)
-data2 = g.text
-for k, v in enumerate(data2):
-    if v=='l':
-        print("test")
-        while v != ',':
-            print (v)
+    def __init__(self, userJSON, tracked):
+        self.lgn = userJSON['login']
+        self.id = userJSON['id']
+        self.name = userJSON['name']
+        self.location = userJSON['location']
+        self.repos = userJSON['public_repos']
+        self.userSince = userJSON['created_at']
+        self.followers = userJSON['followers']
+        self.following = userJSON['following']
+        self.followers_url = userJSON['followers_url']
+        self.trackedUser = tracked
+        self.following_url = userJSON['following_url']
+        if self.name is None:
+            self.name = self.lgn
+        if self.location is None:
+            self.location = "No location"
+        self.printStat = self.createPrintStat()
+        print(self.name)
+        print(self.id)
+
+    def createPrintStat(self):
+        printStat =      str(self.lgn) + ' Login \n' + \
+                               str(self.id) + ', ID Number\n' + \
+                               self.name + ', Name\n' + \
+                               self.location + ', Location\n'+ \
+                               str(self.repos) + ', Repos\n'+ \
+                               self.userSince + ', UserSince\n'+ \
+                               self.followers_url + ', Followers url\n'+ \
+                               self.following_url + ', Following url\n'+ \
+                               str(self.followers) + ', Followers\n'+ \
+                               str(self.following) + ', Following\n' + \
+                               str(self.trackedUser) + ' Tracked User )'
+        return printStat
+
+
+
+
+def followers_pie_chart(int, int2):
+    labels = 'Followers', 'Following'
+    sizes = [int, int2]
+    fig1, ax1 = plt.subplots()
+    ax1.pie(sizes, labels=labels, autopct='%1.1f%%',
+            shadow=True, startangle=90)
+    ax1.axis('equal')
+    plt.show()
+    return
+
+auth = ('C-Nev', '555abb9471e86d7488b68576993c79f902ca1855')
+r = requests.get('https://api.github.com/users/WhelanB', auth=auth)     #an account with followers and is following people
+print(r.status_code)
+
+userJSON = json.loads(r.text or r.content)
+
+initialUser = user(userJSON, 1)
+followers_pie_chart(initialUser.followers, initialUser.following)
+print(r.text)
+print(initialUser.printStat)
+
+
+
