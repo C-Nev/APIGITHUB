c3d2aad5c0e78cc9bd2877471a64b53952ebddd2
Conor Nevin <conevin@tcd.ie>
2017-12-13 22:53:26 +0000

    Add files via upload
    Added web crawler to find the number of repos in someones social circle
    
    A social circle is both the account following & followed by an individual

--- API.py
+++ API.py
@@ -24,8 +24,7 @@ def percentageFollowback(response):
         followingURL = followingURL[:-13]
     if followersURL.endswith('{/other_user}'):
         followersURL = followersURL[:-13]
-    reposURL = r['repos_url']
-    print(reposURL)
+
 
     t = requests.get(followingURL).json()
     y = 0
@@ -48,19 +47,56 @@ def percentageFollowback(response):
         biggestList = len(followersList)
     matchingFollowersSet = set(followersList) & set(followingList)
     percentageFollowbacks = (len(matchingFollowersSet) / biggestList) * 100
-    objects = 'Followback ratio'
+    objects = ''
     y_pos = np.arange(1)
     performance = [100, percentageFollowbacks]
 
     plt.bar(y_pos, performance, align='center', alpha=0.5)
     plt.xticks(y_pos, objects)
-    plt.ylabel('Usage')
-    plt.title('Programming language usage')
+    plt.ylabel('%')
+    plt.title('Ratio of followbacks')
 
     plt.show()
-    print(percentageFollowbacks)
+    return
 
+def repos(response):
+    y = 0
+    reposURL = r['repos_url']
+    followingURL = r['following_url']
+    followersURL = r['followers_url']
+    if followingURL.endswith('{/other_user}'):
+        followingURL = followingURL[:-13]
+    if followersURL.endswith('{/other_user}'):
+        followersURL = followersURL[:-13]
+
+
+
+    t = requests.get(reposURL).json()
+    print (len(t))
+    t = requests.get(followingURL).json()
+    print (t)
+    y = 0
+    followingList = []
+    followersList = []
+    while y < len(t):
+        name = t[y]['repos_url']
+        followingList.append(name + ', ')
+        y += 1
+
+    for x in range(0, len(followingList)):
+        if followingList[x].endswith(', '):
+            followingList[x] = followingList[x][:-2]
+
+    d=0
+    print(followingList)
+    while d<len(followingList):
+
+        t = requests.get(followingList[d]).json()
+        print(len(t))
+        d+=1
+    return
 auth = ('C-Nev', '555abb9471e86d7488b68576993c79f902ca1855')
 r = requests.get('https://api.github.com/users/WhelanB', auth=auth).json()     #an account with followers and is following people
-percentageFollowback(r)
-followers_pie_chart(r)
+#percentageFollowback(r)
+#followers_pie_chart(r)
+repos(r)
