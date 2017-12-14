ee31f97fbcd9056c8ec33a084f82d380f27e0fee
Conor Nevin <conevin@tcd.ie>
2017-12-14 17:50:57 +0000

    More work
    Added more charts and a simple web crawler though I'm not sure if I added that last night or this morning

--- API.py
+++ API.py
@@ -4,6 +4,7 @@ import json
 import random
 import matplotlib.pyplot as plt
 import numpy as np
+import networkx as nx
 
 def followers_pie_chart(response):
     followers = r['followers']
@@ -51,33 +52,31 @@ def percentageFollowback(response):
     y_pos = np.arange(1)
     performance = [100, percentageFollowbacks]
 
-    plt.bar(y_pos, performance, align='center', alpha=0.5)
+    plt.bar(y_pos, performance, align='center',label='Followbacks', alpha=0.5)
     plt.xticks(y_pos, objects)
     plt.ylabel('%')
+    plt.xlabel(str(r['login']))
     plt.title('Ratio of followbacks')
 
     plt.show()
     return
 
 def repos(response):
-    y = 0
+
+    reposList = []
     reposURL = r['repos_url']
     followingURL = r['following_url']
-    followersURL = r['followers_url']
     if followingURL.endswith('{/other_user}'):
         followingURL = followingURL[:-13]
-    if followersURL.endswith('{/other_user}'):
-        followersURL = followersURL[:-13]
 
 
 
-    t = requests.get(reposURL).json()
-    print (len(t))
-    t = requests.get(followingURL).json()
-    print (t)
+    t = requests.get(reposURL, auth=auth).json()
+    t = requests.get(followingURL, auth=auth).json()
+
     y = 0
     followingList = []
-    followersList = []
+    followingLista = []
     while y < len(t):
         name = t[y]['repos_url']
         followingList.append(name + ', ')
@@ -88,15 +87,87 @@ def repos(response):
             followingList[x] = followingList[x][:-2]
 
     d=0
-    print(followingList)
     while d<len(followingList):
+        p = requests.get(followingList[d], auth=auth).json()
+        j = t[d]['login']
+        followingLista.append(len(p))
+        reposList.append(str(j) + ', ')
 
-        t = requests.get(followingList[d]).json()
-        print(len(t))
         d+=1
+    followingTuple = zip(followingLista, reposList)
+    followingTuple = sorted(followingTuple)
+    followingLista =([ a for a,b in followingTuple ], [ b for a,b in followingTuple ])
+    n_groups = len(followingList)
+    reposNum = followingLista[0]
+
+    fig, ax = plt.subplots()
+
+    index = np.arange(n_groups)
+    bar_width = 0.35
+
+    opacity = 0.4
+    error_config = {'ecolor': '0.3'}
+
+    rects1 = plt.bar(index, reposNum, bar_width,
+                     alpha=opacity,
+                     color='b',
+                     error_kw=error_config,
+                     label='Repos')
+
+    plt.xlabel('Login')
+    plt.ylabel('Repos')
+    plt.title('Number of repos in social circle of '+str(r['login']))
+    plt.xticks(index + bar_width / 2, followingLista[1])
+    plt.legend()
+
+    plt.tight_layout()
+    plt.show()
+    return
+
+def socialGraph(response):
+    followingURL = r['following_url']
+    followersURL = r['followers_url']
+    if followingURL.endswith('{/other_user}'):
+        followingURL = followingURL[:-13]
+    if followersURL.endswith('{/other_user}'):
+        followersURL = followersURL[:-13]
+
+    t = requests.get(followingURL).json()
+    s = requests.get(followersURL).json()
+    y = 0
+    followingList = []
+    followersList = []
+    while y < len(t):
+        name = t[y]['login']
+        followingList.append(name)
+        y += 1
+    y =0
+    while y < len(s):
+        namef = s[y]['login']
+        followersList.append(namef)
+        y += 1
+    y=0
+    print(followersList)
+    print(followingList)
+    G = nx.path_graph(followingList)
+    G.add_node((str(r['login'])))
+    G.add_nodes_from(followingList)
+    G.add_nodes_from(followersList)
+    while y<len(followingList):
+        G.add_edges_from(
+            [(str(r['login']), followingList[y])])
+        y += 1
+
+    while y < len(followersList):
+          G.add_edges_from(
+          [(str(r['login']), followersList[y])])
+          y += 1
+    nx.draw(G)
+    plt.show()
     return
 auth = ('C-Nev', '555abb9471e86d7488b68576993c79f902ca1855')
-r = requests.get('https://api.github.com/users/WhelanB', auth=auth).json()     #an account with followers and is following people
+r = requests.get('https://api.github.com/users/'+str(auth[0]), auth=auth).json()     #an account with followers and is following people
 #percentageFollowback(r)
 #followers_pie_chart(r)
-repos(r)
+#repos(r)
+socialGraph(r)
